522f7bd66737875a6f6a3fe2856895268bf1718d
Shazron Abdullah <shazron@apache.org>
2015-05-17 01:03:24 -0700

    Incremented version to 1.7.0

--- ios-deploy.c
+++ ios-deploy.c
@@ -18,7 +18,7 @@
 #include "MobileDevice.h"
 #include "errors.h"
 
-#define APP_VERSION    "1.6.3"
+#define APP_VERSION    "1.7.0"
 #define PREP_CMDS_PATH "/tmp/fruitstrap-lldb-prep-cmds-"
 #define LLDB_SHELL "lldb -s " PREP_CMDS_PATH
 /*
